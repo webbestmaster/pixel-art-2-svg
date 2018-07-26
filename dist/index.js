be3f1f987da507e64c8cefe3fd821ae50d321a68
turovtsov <dturovtsov@marathonbet.ru>
2018-07-26 17:45:44 +0300

    fix executing

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,5 @@
 'use strict';
 
-Object.defineProperty(exports, '__esModule', {
-    value: true
-});
-
-var _regenerator = require('babel-runtime/regenerator');
-
-var _regenerator2 = _interopRequireDefault(_regenerator);
-
-var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
-
-var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
-
 var _promise = require('babel-runtime/core-js/promise');
 
 var _promise2 = _interopRequireDefault(_promise);
@@ -89,46 +77,12 @@ function imageDataToSvg(imageData) {
     return svgString.replace('{data}', rectList.join(''));
 }
 
-exports.default = (function() {
-    var _ref = (0, _asyncToGenerator3.default)(
-        /*#__PURE__*/ _regenerator2.default.mark(function _callee(pathToImage) {
-            var imageData;
-            return _regenerator2.default.wrap(
-                function _callee$(_context) {
-                    while (1) {
-                        switch ((_context.prev = _context.next)) {
-                            case 0:
-                                _context.next = 2;
-                                return getImageData(pathToImage);
+module.exports = function imgToSvg(pathToImage) {
+    return getImageData(pathToImage).then(function(imageData) {
+        if (!imageData) {
+            return null;
+        }
 
-                            case 2:
-                                imageData = _context.sent;
-
-                                if (imageData) {
-                                    _context.next = 5;
-                                    break;
-                                }
-
-                                return _context.abrupt('return', null);
-
-                            case 5:
-                                return _context.abrupt('return', imageDataToSvg(imageData));
-
-                            case 6:
-                            case 'end':
-                                return _context.stop();
-                        }
-                    }
-                },
-                _callee,
-                this
-            );
-        })
-    );
-
-    function imgToSvg(_x) {
-        return _ref.apply(this, arguments);
-    }
-
-    return imgToSvg;
-})();
+        return imageDataToSvg(imageData);
+    });
+};
